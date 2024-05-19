4bd0606c9705f8c9e560b1277f4d3379b39126f1
Quanghuy128 <quanghuyhihi128@gmail.com>
2024-05-19 10:43:57 +0700

    add middleware & configure swagger

---
+++ EduLingual.Api/Configuration/ConfigureSwaggerOptions.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+using System.Reflection;
+
+namespace EduLingual.Api.Configuration
+{
+    public static class ConfigureSwaggerOptions
+    {
+        public static void AddSwaggerDocumentation(this SwaggerGenOptions options)
+        {
+
+            var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+            var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+            options.IncludeXmlComments(xmlPath);
+        }
+
+        public static IServiceCollection AddSwaggerGenOption(this IServiceCollection service)
+        {
+            return service.AddSwaggerGen(c =>
+            {
+                c.AddSwaggerDocumentation();
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "Edulingual API",
+                    Version = "v1",
+                    Description = "Edulingual API",
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Contact Developers",
+                        Url = new Uri("https://github.com/Quanghuy128/EduLingual")
+                    }
+                });
+                c.AddSecurityDefinition("oauth2", new OpenApiSecurityScheme
+                {
+                    Description = "Standard Authorization header using the Bearer scheme. Example: \"Bearer {token}\"",
+                    In = ParameterLocation.Header,
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.ApiKey
+                });
+            });
+        }
+    }
+}
