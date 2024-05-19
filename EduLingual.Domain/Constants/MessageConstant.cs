bb69aa82b4374d8f3b9480d2babc1a351d0f51c4
huynguyen-vpo <huynguyen-vpo>
2024-05-19 08:57:37 +0700

    add user crud message

--- EduLingual.Domain/Constants/MessageConstant.cs
+++ EduLingual.Domain/Constants/MessageConstant.cs
@@ -4,19 +4,26 @@
     {
         public static class Vi
         {
-            #region User Field
-            private const string UserName = "Tên đăng nhập";
-            private const string Password = "Mật khẩu";
-            private const string FullName = "Tên người dùng";
-            private const string Description = "Mô tả";
-            private const string UserStatus = "Trạng thái";
-            #endregion
+            #region Template, Suffix, Prefix
+            private const string CreateSuccessTemplate = "Tạo mới {0} thành công !!!";
+            private const string UpdateSuccessTemplate = "Cập nhật {0} thành công !!!";
+            private const string DeleteSuccessTemplate = "Xóa {0} thành công !!!";
+            private const string CreateFailTemplate = "Tạo mới {0} thất bại @.@";
+            private const string UpdateFailTemplate = "Cập nhật {0} thất bại @.@";
+            private const string DeleteFailTemplate = "Xóa {0} thất bại @.@";
 
-            #region Suffix
             private const string RequiredSuffix = " không được bỏ trống !!!";
             #endregion
             public static class User
             {
+                #region User Field
+                private const string UserMessage = "người dùng";
+                private const string UserName = "Tên đăng nhập";
+                private const string Password = "Mật khẩu";
+                private const string FullName = "Tên người dùng";
+                private const string Description = "Mô tả";
+                private const string UserStatus = "Trạng thái";
+                #endregion
                 public static class Require
                 {
                     public const string UserNameRequired = UserName + RequiredSuffix;
@@ -25,6 +32,18 @@
                     public const string DescriptionRequired = Description + RequiredSuffix;
                     public const string StatusRequired = UserStatus + RequiredSuffix;
                 }
+                public static class Success
+                {
+                    public static string CreateUser = String.Format(CreateSuccessTemplate, UserMessage);
+                    public static string UpdateUser = String.Format(UpdateSuccessTemplate, UserMessage);
+                    public static string DeleteUser = String.Format(DeleteSuccessTemplate, UserMessage);
+                }
+                public static class Fail
+                {
+                    public static string CreateUser = String.Format(CreateFailTemplate, UserMessage);
+                    public static string UpdateUser = String.Format(UpdateFailTemplate, UserMessage);
+                    public static string DeleteUser = String.Format(DeleteFailTemplate, UserMessage);
+                }
             }
             public static class Course
             {
@@ -32,10 +51,26 @@
                 {
 
                 }
+                public static class Success
+                {
+
+                }
+                public static class Fail
+                {
+
+                }
             }
             public static class Feedback
             {
                 public static class Require
+                {
+
+                }
+                public static class Success
+                {
+
+                }
+                public static class Fail
                 {
 
                 }
