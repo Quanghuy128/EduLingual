f33d126adb607eeaa0b557cb430c8ed6e4caef2b
Quansucno <113964643+Quanntn0811@users.noreply.github.com>
2024-05-20 19:58:02 +0700

    crud course language

--- EduLingual.Application/Service/ICourseLanguageService.cs
+++ EduLingual.Application/Service/ICourseLanguageService.cs
@@ -1,12 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using EduLingual.Domain.Common;
+using EduLingual.Domain.Dtos.CourseArea;
+using EduLingual.Domain.Dtos.CourseLanguage;
+using EduLingual.Domain.Entities;
+using System.Linq.Expressions;
 
 namespace EduLingual.Application.Service
 {
     public interface ICourseLanguageService
     {
+        Task<Result<List<CourseLanguageViewModel>>> GetAll(Expression<Func<CourseLanguage, bool>>? predicate);
+        Task<PagingResult<CourseLanguageViewModel>> GetPagination(Expression<Func<CourseLanguage, bool>>? predicate, int page, int size);
+        Task<Result<CourseLanguageViewModel>> Get(Guid id);
+        Task<Result<CourseLanguageViewModel>> Create(CreateCourseLanguageRequest request);
+        Task<Result<bool>> Update(Guid id, UpdateCourseLanguageRequest request);
+        Task<Result<bool>> Delete(Guid id);
     }
 }
