435aaf3296e6e449fb3d3776347c100e49890e90
Quansucno <113964643+Quanntn0811@users.noreply.github.com>
2024-06-06 15:31:41 +0700

    filter role user

---
+++ EduLingual.Domain/Dtos/Role/RoleDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EduLingual.Domain.Dtos.Role
+{
+    public class RoleDto
+    {
+        public Guid Id { get; set; } 
+        public string RoleName { get; set; }
+    }
+}
