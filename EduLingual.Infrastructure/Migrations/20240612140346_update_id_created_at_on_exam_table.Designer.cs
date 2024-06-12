ad78ca0098f73faddde88021268d09e54e311a32
Quansucno <113964643+Quanntn0811@users.noreply.github.com>
2024-06-12 21:04:52 +0700

    update fields on table exam

--- EduLingual.Infrastructure/Migrations/20240612140346_update_id_created_at_on_exam_table.Designer.cs
+++ EduLingual.Infrastructure/Migrations/20240612140346_update_id_created_at_on_exam_table.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using EduLingual.Infrastructure;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace EduLingual.Infrastructure.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240612140346_update_id_created_at_on_exam_table")]
+    partial class update_id_created_at_on_exam_table
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ EduLingual.Infrastructure/Migrations/20240612140346_update_id_created_at_on_exam_table.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EduLingual.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class update_id_created_at_on_exam_table : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_user_exam",
+                schema: "edl",
+                table: "user_exam");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "id",
+                schema: "edl",
+                table: "user_exam",
+                type: "uuid",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "created_at",
+                schema: "edl",
+                table: "user_exam",
+                type: "timestamp without time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_user_exam",
+                schema: "edl",
+                table: "user_exam",
+                column: "id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_user_exam_exam_id",
+                schema: "edl",
+                table: "user_exam",
+                column: "exam_id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_user_exam",
+                schema: "edl",
+                table: "user_exam");
+
+            migrationBuilder.DropIndex(
+                name: "IX_user_exam_exam_id",
+                schema: "edl",
+                table: "user_exam");
+
+            migrationBuilder.DropColumn(
+                name: "id",
+                schema: "edl",
+                table: "user_exam");
+
+            migrationBuilder.DropColumn(
+                name: "created_at",
+                schema: "edl",
+                table: "user_exam");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_user_exam",
+                schema: "edl",
+                table: "user_exam",
+                columns: new[] { "exam_id", "user_id" });
+        }
+    }
+}
