2170ed8bde13429696bd1fb1f74c348b4fd5317c
Quansucno <113964643+Quanntn0811@users.noreply.github.com>
2024-06-21 14:56:56 +0700

    change description type table course

---
+++ EduLingual.Infrastructure/Migrations/20240621075345_change_desciption_type_table_course.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EduLingual.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class change_desciption_type_table_course : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "description",
+                schema: "edl",
+                table: "course",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(1000)",
+                oldMaxLength: 1000);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "description",
+                schema: "edl",
+                table: "course",
+                type: "character varying(1000)",
+                maxLength: 1000,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+        }
+    }
+}
